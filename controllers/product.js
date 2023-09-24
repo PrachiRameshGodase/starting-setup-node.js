f0e0d8efc0508e75e287136b443ee3d677c5ffd6
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-09-24 15:16:44 +0530

    using controller for 404 & model product

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product=require("../models/products")
 exports.getAddproduct=(req, res, next) => {
     res.render('add-product', {
       pageTitle: 'Add Product',
@@ -10,12 +10,13 @@ exports.getAddproduct=(req, res, next) => {
 }
   
 exports.postAddProduct=(req, res, next) => {
-    products.push({ title: req.body.title });
+   const product=new Product(req.body.title)
+   product.save()
     res.redirect('/');
   }
 
   exports.getProduts=(req, res, next) => {
-   
+   const products=Product.fetchAll()
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
